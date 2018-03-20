c769f7e37c3e9f0a2900240ce5e4bed426d7c868
lipanshan <15003167565@163.com>
2018-03-20 12:39:09 +0800

    update vue-cli and add myweb project

---
+++ musicplayer/prod.sever.js
@@ -0,0 +1,73 @@
+var express = require('express')
+var config = require('./config/index')
+var axios = require('axios')
+
+var port = process.env.PORT || config.build.port
+
+var app = express()
+
+var apiRoutes = express.Router()
+
+apiRouter.get('/hotrecommendlist', function (req, res) {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'http://y.qq.com/'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+apiRouter.get('/getVkey', function (req, res) {
+  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/portal/player.html'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+apiRouter.get('/getLyric', function (req, res) {
+  const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/portal/player.html'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+apiRouter.get('/getRankGroupList', function (req, res) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_opt.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/n/yqq/toplist/4.html'
+    },
+    params: req.query
+  }).then((response) => {
+    res.send(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+app.use('/api', apiRoutes)
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port, function(err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
