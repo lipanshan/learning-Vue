aaae9f87becf50ae636dd52c1249eadf0bf93633
lipanshan <15003167565@163.com>
2018-05-21 17:44:03 +0800

    add certificate and comment page

--- myweb2/src/components/certificate.vue
+++ myweb2/src/components/certificate.vue
@@ -1,10 +1,98 @@
 <template>
   <div class="certificate-wrap">
-    证书
+    <div class="min-avatar-wrap">
+      <div class="avatar-box">
+        <img src="static/image/avatar.jpg" alt="头像">
+      </div>
+      <h1 class="name">牧歌</h1>
+    </div>
+    <div class="resulte-wrap">
+      <div class="split-line"></div>
+      <p class="txt">恭喜您</p>
+      <p class="name">【共建高校团队】</p>
+      <p class="txt2">课程学习成绩为<span class="red">95分</span></p>
+      <div class="result">合格</div>
+    </div>
+    <div class="time-wrap">
+      <p class="text">学习时间</p>
+      <p class="time">2017年10月10日00:00至2017年10月10日00:00</p>
+    </div>
+    <div class="background">
+    </div>
   </div>
 </template>
 <style lang="sass" type="text/css" rel="stylesheet/sass">
-
+@import '../common/sass/variable'
+.certificate-wrap
+  position: relative
+  .background
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background: url(../common/image/certificate-bg.jpg) no-repeat 0 0
+    background-size: 100% 100%
+    z-index: -1
+  .min-avatar-wrap
+    margin-top: 94px
+    .avatar-box
+      margin: 0 auto
+      width: 73px
+      height: 73px
+      border-radius: 50%
+      overflow: hidden
+      &>img
+        width: 73px
+        height: 73px
+  .name
+    font-size: $font-size-small
+    line-height: $font-size-small + 14px
+    color: $color-text
+    text-align: center
+  .resulte-wrap
+    margin-top: 13px
+    .split-line
+      margin: 0 88px
+      height: 15px
+      overflow: hidden
+      background: url(../common/image/split-line.png) no-repeat 0 0
+      background-size: 100% 15px
+    .txt
+      font-size: $font-size-small-l
+      line-height: $font-size-small-l + 22px
+      color: $color-text
+      text-align: center
+    .name
+      font-size: $font-size-medium-s
+      line-height: $font-size-medium-s + 16px
+      color: $color-text-d
+      text-align: center
+    .txt2
+      @extend .name
+      color: $color-text
+      &>.red
+        color: $color-text-red
+    .result
+      margin: 39px auto 0
+      width: 194px
+      height: 45px
+      background: url(../common/image/border.png) no-repeat 0 0px
+      background-size: 194px 45px
+      font-size: $font-size-medium-m
+      line-height: $font-size-medium-m + 27px
+      color: $color-text-red
+      text-align: center
+  .time-wrap
+    margin-top: 50px
+    .text
+      font-size: $font-size-small-m
+      line-height: $font-size-small-m + 8px
+      color: $color-text-d
+      text-align: center
+    .time
+      @extend .text
+      font-size: $font-size-small-s
 </style>
 <script type="text/ecmascript-6">
 export default {
--- myweb2/src/components/comment.vue
+++ myweb2/src/components/comment.vue
@@ -1,33 +1,81 @@
 <template>
   <div class="comment-wrap">
-    <v-scroll class="form">
+    <v-scroll class="form" ref="commentScrollWrap">
       <div class="box">
         <consult-item :itemInfo="list[0]"></consult-item>
       </div>
       <div class="title-header border-bottom-1px">
-        <div class="title-wrap active">
+        <div @click="selectTag(0)" class="title-wrap" :class="{'active': currentIndex === 0}">
           <h2>课程评价</h2>
         </div>
-        <div class="title-wrap">
+        <div  @click="selectTag(1)"  class="title-wrap" :class="{'active': currentIndex === 1}">
           <h2>课程作业</h2>
         </div>
       </div>
-      <div class="comment-box">
+      <div v-show="currentIndex === 0" class="comment-box">
         <ul class="star-wrap">
-          <li class="star-item">
-            <p class="txt">内容评价</p>
-            <ul class="star-list">
-              <li class="star"><i class="icon-star-empty"></i></li>
-              <li class="star"><i class="icon-star-empty"></i></li>
-              <li class="star"><i class="icon-star-empty"></i></li>
-              <li class="star"><i class="icon-star-empty"></i></li>
-              <li class="star"><i class="icon-star-empty"></i></li>
-            </ul>
+          <li v-for="item of starList" :key="item.title" class="star-item border-bottom-1px">
+            <p class="txt">{{item.title}}</p>
+            <star :star="item" @setStar="setStar"></star>
           </li>
         </ul>
+        <h3>对您最有帮助:</h3>
+        <div class="textarea-wrap">
+          <textarea v-model="textarea1" placeholder="请输入您要评价的信息"></textarea>
+        </div>
+        <h3>意见/建议:</h3>
+        <div class="textarea-wrap">
+          <textarea v-model="textarea2" placeholder="请输入您要评价的信息"></textarea>
+        </div>
+      </div>
+      <div v-show="currentIndex === 1" class="comment-box">
+        <div class="course-work">
+          <div class="file-wrap">
+            <h2 class="title">学习心得</h2>
+            <div class="file-box">
+              <cube-upload
+                ref="upload"
+                v-model="files"
+                :action="action"
+                @files-added="addedHandler"
+                @file-error="errHandler">
+                <div class="clear-fix">
+                  <cube-upload-file v-for="(file, i) in files" :file="file" :key="i"></cube-upload-file>
+                  <cube-upload-btn :multiple="false">
+                    <div>
+                      <i class="icon icon-folder-plus"></i>
+                      <p>上传文件</p>
+                    </div>
+                  </cube-upload-btn>
+                </div>
+              </cube-upload>
+            </div>
+          </div>
+          <div class="picture-wrap">
+            <h2 class="title">分享截图</h2>
+            <div class="picture-box">
+              <cube-upload
+                ref="upload2"
+                v-model="files2"
+                :action="action"
+                @files-added="addedHandler2"
+                @file-error="errHandler2">
+                <div class="clear-fix">
+                  <cube-upload-file v-for="(file, i) in files2" :file="file" :key="i"></cube-upload-file>
+                  <cube-upload-btn :multiple="false">
+                    <div>
+                      <i class="icon icon-photo"></i>
+                      <p>上传图片</p>
+                    </div>
+                  </cube-upload-btn>
+                </div>
+              </cube-upload>
+            </div>
+          </div>
+        </div>
       </div>
     </v-scroll>
-    <div class="submit">保存</div>
+    <div @click="submit" class="submit">保存</div>
   </div>
 </template>
 <style lang="sass" type="text/css" rel="stylesheet/sass">
@@ -40,7 +88,11 @@
   left: 0
   z-index: 10
   .form
-    height: 100%
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 48px
+    left: 0
     overflow: hidden
     .box
       padding: 0 15px
@@ -53,7 +105,6 @@
       .title-wrap
         flex: 1
         font-size: 0
-        line-height: 44px
         text-align: center
         &.active
           &>h2
@@ -66,6 +117,101 @@
           line-height: $font-size-small-l + 30px
           border-bottom: 2px solid transparent
           color: $color-text
+    .comment-box
+      padding-bottom: 17px
+      .star-wrap
+        padding-left: 15px
+        padding-right: 15px
+        background-color: $color-theme
+        .star-item
+          position: relative
+          display: flex
+          &>.txt
+            padding-top: 1px
+            flex: 1
+            font-size: $font-size-small-l
+            line-height: $font-size-small-l + 34px
+            color: $color-text-l
+          &>.star-list
+            flex: 1
+      &>h3
+        padding-left: 15px
+        padding-right: 15px
+        font-size: $font-size-small-l
+        line-height: $font-size-small-l + 30px
+        color: $color-text-l
+      .textarea-wrap
+        display: flex
+        padding: 0 15px
+        border-radius: 4px
+        &>textarea
+          flex: 1
+          display: block
+          padding: 11px 18px
+          resize: none
+          border-radius: 4px
+          border: none
+          color: $color-text
+          font-size: $font-size-small-l
+          line-height: $font-size-small-l + 10px
+          background-color: $color-theme
+    .course-work
+      display: flex
+      padding-left: 15px
+      padding-right: 15px
+      .picture-wrap,.file-wrap
+        flex: 1
+        .title
+          padding-top: 14px
+          padding-bottom: 14px
+          font-size: $font-size-small
+          line-height: $font-size-small + 10px
+          text-align: center
+          color: $color-text-d
+        .title:nth-child(1)
+          padding-right: 6px
+        .title:nth-child(2)
+          padding-left: 6px
+        .picture-box,.file-box
+          background-color: $color-theme
+          border-radius: 4px
+          .cube-upload
+            .cube-upload-file, .cube-upload-btn
+              margin: 0
+              height: 77px
+            .cube-upload-file
+              margin: 0
+              + .cube-upload-btn
+                margin-top: -77px
+                opacity: 0
+            .cube-upload-file-def
+              width: 100%
+              height: 100%
+              background-size: 100% 77px
+              .cubeic-wrong
+                display: none
+            .cube-upload-btn
+              display: flex
+              align-items: center
+              justify-content: center
+              &> div
+                text-align: center
+                color: $color-text
+              i
+                display: inline-flex
+                align-items: center
+                justify-content: center
+                width: 50px
+                height: 50px
+                font-size: 32px
+                line-height: 1
+                font-style: normal
+                border-radius: 50%
+                color: $color-text
+        .file-box
+          margin-right: 6px
+        .picture-box
+          margin-left: 6px
   .submit
     position: fixed
     right: 0
@@ -81,6 +227,8 @@
 <script type="text/ecmascript-6">
 import consultItem from '@/components/consultitem'
 import vScroll from '@/components/scroll'
+import star from '@/components/star'
+import cube from 'cube-ui'
 export default {
   data () {
     return {
@@ -95,12 +243,97 @@ export default {
           date: '9月28日 12:00-14:00',
           idAddress: true
         }
-      ]
+      ],
+      starList: [
+        {
+          title: '内容评价',
+          star: 0
+        },
+        {
+          title: '授课水平',
+          star: 0
+        },
+        {
+          title: '场地及设施安排',
+          star: 0
+        },
+        {
+          title: '培训的进程安排',
+          star: 0
+        },
+        {
+          title: '日后工作的帮助',
+          star: 0
+        },
+        {
+          title: '总体评价',
+          star: 0
+        }
+      ],
+      currentIndex: 0,
+      action: '',
+      files: [],
+      files2: [],
+      textarea1: '',
+      textarea2: '',
+      info: {}
+    }
+  },
+  methods: {
+    selectTag (num) {
+      this.currentIndex = num
+    },
+    setStar (data) {
+      this.starList.forEach((item) => {
+        if (data.title === item.title) {
+          item.star = data.starNum
+        }
+      })
+    },
+    addedHandler () {
+      const file = this.files[0]
+      file && this.$refs.upload.removeFile(file)
+    },
+    addedHandler2 () {
+      const file = this.files2[0]
+      file && this.$refs.upload2.removeFile(file)
+    },
+    errHandler (file) {
+      // const msg = file.response.message
+      this.$createToast({
+        type: 'warn',
+        txt: 'Upload fail',
+        time: 1000
+      }).show()
+    },
+    errHandler2 (file) {
+      // const msg = file.response.message
+      this.$createToast({
+        type: 'warn',
+        txt: 'Upload fail',
+        time: 1000
+      }).show()
+    },
+    submit () {
+      this.info = Object.assign({}, {
+        star: this.starList,
+        file1: this.files,
+        file2: this.files2,
+        textarea1: this.textarea1,
+        textarea2: this.textarea1
+      })
+      this.$createDialog({
+        type: 'warn',
+        content: `<span class="word-break">${JSON.stringify(this.info)}</span>`,
+        icon: 'cubeic-alert'
+      }).show()
     }
   },
   components: {
     vScroll,
-    consultItem
+    consultItem,
+    star,
+    cube
   }
 }
 </script>
